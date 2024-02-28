69986d5bd4f87aa0b34722f9502a9ecfd3a08cb1
smallrussian <julian.shniter@gmail.com>
2024-02-28 13:40:19 -0600

    firist commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Header from "./Header.svelte";
+	import Calculator from "./Calculator.svelte";
+	</script>
+	
+	<style>
+	  main {
+		font-family: sans-serif;
+		text-align: center;
+	  }
+	</style>
+	
+	<main>
+		<Header />
+	  <Calculator />
+	</main>
+	
---
+++ src/routes/Header.svelte
@@ -0,0 +1,11 @@
+<style>
+    header{
+        background-color: #f4f4f4;
+        padding: 20px 0;
+        text-align: center;
+    }
+</style>
+
+<header>
+    <h1>Calculator</h1>
+</header>
